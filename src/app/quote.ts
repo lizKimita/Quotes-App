8b2ff4cc5bc8b2c397a6a57147251f0bc808407e
lizKimita <kimita.wanjiku@gmail.com>
2019-03-22 15:18:18 +0300

    adding date feature in the app and a date count pipe

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDetails:boolean;
-    constructor (public id:number, public name:string, public message:string, public author: string, public votes:number){
+    constructor (public id:number, public name:string, public message:string, public author: string, public votes:number, public inputDate:Date){
         this.showDetails=false
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1,'Shiko','Start every day off with a smile and get it over with!','W.C Fields', 10),
-    new Quote(3,'Glamo','If you are going through hell, keep going.', 'Winston Churchill', 20),
-    new Quote(4,'Kittony','And so ends another week without me getting rich unexpectedly.','Anonymous',7),
-    new Quote(5,'Kijo','Insanity: doing the same thing over and over again and expecting different results.','Albert Einstein',0),
-    new Quote(6,'Grace','I think it is good that books still exist, but they do make me sleepy.','Frank Zappa',4),
-    new Quote(7,'Asenath','I would like to live like a poor man,only with lots of money.','Pablo Picasso',100),
+    new Quote(1,'Shiko','Start every day off with a smile and get it over with!','W.C Fields', 10, new Date(2018,10,12)),
+    new Quote(3,'Glamo','If you are going through hell, keep going.', 'Winston Churchill', 20, new Date(2019,0,12)),
+    new Quote(4,'Kittony','And so ends another week without me getting rich unexpectedly.','Anonymous',7, new Date(2019,3,1)),
+    new Quote(5,'Kijo','Insanity: doing the same thing over and over again and expecting different results.','Albert Einstein',0, new Date(2018,11,25)),
+    new Quote(6,'Grace','I think it is good that books still exist, but they do make me sleepy.','Frank Zappa',4, new Date(2019,5.28)),
+    new Quote(7,'Asenath','I would like to live like a poor man,only with lots of money.','Pablo Picasso',100, new Date(2018,5,28)),
 ]
 
 revealDetails(i){
